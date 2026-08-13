91b569927acb063c2b1622157743374ace7309d0
Ajit <ajiteshgusain@gmail.com>
2026-08-13 09:53:13 +0530

    Add math functions for sum and multiplication

---
+++ server.js
@@ -0,0 +1,10 @@
+const sum=require('./math').sum
+const total=sum(10,20);
+
+console.log(`sum of the numbers:${total}`);
+
+
+const multiply=require('./math').multiply
+const product=multiply(10,20);
+
+console.log(`product of the numbers:${product}`);
