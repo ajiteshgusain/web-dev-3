18af685b8bf776f83d0ab71a2b61c03e65c702e3
Ajit <ajiteshgusain@gmail.com>
2026-08-31 13:31:07 +0530

    commit

--- class3/server.js
+++ class3/server.js
@@ -1,8 +1,26 @@
 import  http from  'http'//  importing    http module(built-in)
 
 const server=http.createServer((req,res)=>{
-    if (req.url==='/' && req.method=='GET'){
-        res.end('<h1> welcome to  backend.</h1>')
+    // if (req.url==='/' && req.method=='GET'){
+    //     res.end('<h1> welcome to  backend.</h1>')
+    // }
+
+    let  body=''
+    if(req.url==='user' && req.method ==='POST'){
+        req.on('data',(chunk)=>{
+            body=body+chunk
+
+
+        })
+
+        req.on('end',()=>{
+            res.writeHead(200,{'content-type':'application/json'})
+            res.end(JSON.stringify({
+                message:'data fetched successfully...',
+                body,
+                success:true
+            }))
+        })
     }
  
 })  
