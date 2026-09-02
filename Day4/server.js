4fd224aaa2b42b9f0a4c0f30602080b109842d5b
Ajit <ajiteshgusain@gmail.com>
2026-09-02 10:05:13 +0530

    commit

--- Day4/server.js
+++ Day4/server.js
@@ -2,11 +2,21 @@ import   express from  'express'
 
 const app=express()
 
+app.use(express.json())
 app.get('/',(req,res)=>{
-    res.send('<h1> welcome to  backend')
-}
-)
+    //res.send('<h1> welcome to express backend')
+    res.json({
+        message:'welcome  to express'
+    })
 
+
+})
+
+
+app.post('/user',(req,res)=>{
+    let name=req.body.name
+
+})
 
 app.listen(3000,()=>{
     console.log('server created at port',3000)
