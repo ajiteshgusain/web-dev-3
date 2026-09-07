82358f5d88653781526ae6e4a39c9e20b9a5a935
Ajit <ajiteshgusain@gmail.com>
2026-09-07 13:15:12 +0530

    commit

--- Day4/server.js
+++ Day4/server.js
@@ -2,26 +2,35 @@ import   express from  'express'
 
 const app=express()
 
-app.use(express.json())
-app.get('/',(req,res)=>{
-    //res.send('<h1> welcome to express backend')
-    res.json({
-        message:'welcome  to express'
-    })
+// app.use(express.json())
+// app.get('/',(req,res)=>{
+//     //res.send('<h1> welcome to express backend')
+//     res.json({
+//         message:'welcome  to express'
+//     })
 
 
-})
+// })
 
 
-app.post('/user',(req,res)=>{
-    let name=req.body.name
-    console.log(name)
+// app.post('/user',(req,res)=>{
+//     let name=req.body.name
+//     console.log(name)
 
+//     res.status(200).json({
+//         message:'data recieved successfully...',
+//         success:true
+//     })
+
+// })
+
+let user=['ajitesh','abhi','mummy','papa']
+
+app.get('/getuser',(res,req)=>{
     res.status(200).json({
-        message:'data recieved successfully...',
-        success:true
+        message:'data  recived  successfully...',
+        user
     })
-
 })
 
 app.listen(3000,()=>{
