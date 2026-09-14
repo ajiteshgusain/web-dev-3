cbfa757c56beb878f6472aa6a33602df120786a4
Ajit <ajiteshgusain@gmail.com>
2026-09-14 21:00:58 +0530

    commit

--- Day5/server.js
+++ Day5/server.js
@@ -1,12 +1,12 @@
-import  express  from "express"
+// import  express  from "express"
 
-const app=express()
-import router from './routes/userRoute'
-app.use(route)
+// const app=express()
+// import router from './routes/userRoute'
+// app.use(route)
 
-const port=3000
-app.listen(port,()=>{
-    console.log('server  has at  port:',port)
+// const port=3000
+// app.listen(port,()=>{
+//     console.log('server  has at  port:',port)
 
-})
+// })
 
