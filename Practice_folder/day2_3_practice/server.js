e6fa7a20a288ec1fb8e21573c5afc2a0d945999f
Ajit <ajiteshgusain@gmail.com>
2026-09-26 15:52:36 +0530

    commit

---
+++ Practice_folder/day2_3_practice/server.js
@@ -0,0 +1,46 @@
+// // let  operation=process.argv[2]
+// // let  num1=Number(process.argv[3])
+// // let  num2=Number(process.argv[4])
+
+
+// // // Index 0 and 1 are hidden system paths (Node itself and your file path).
+
+// // // Index 2, 3, and 4 are your custom inputs!
+
+
+
+// // if (operation === 'add'){
+// //     console.log(num1+num2)
+// // }else if(operation === 'sub'){
+// //     console.log(num1-num2)
+// // }
+
+
+
+
+// //----------------------------------------------------------------------------------------
+
+// //  starting    the  HTTP  Server(http.createServer)
+
+// import http  from 'http'
+
+// const server=http.createServer((req,res)=>{
+//     if(req.url==='/' &&  req.method === 'GET'){
+//         res.end('<h1>welcome  to backend</h1>')
+    
+//     }else if(req.url === '/about' && req.method ==='GET'){
+//         res.end('<h1> this is my first  page  created using  backend</h1>')
+
+//     }else{
+//         res.end('page not found')
+//     }
+
+// })
+
+// const port=3000;
+
+// server.listen(port,()=>{
+//     console.log('server  has   started at port:->',port)
+// })
+//---------------------------------------------------------------------------
+// day3
